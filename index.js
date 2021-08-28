5df60b3a67229a6ddd83c99216278a0eef11bce3
muhdwaseem <waseemvann@gmail.com>
2021-08-29 02:10:34 +0530

    docs

--- index.js
+++ index.js
@@ -1,19 +1,38 @@
 const dateofBirth = document.querySelector("#date-of-birth");
 const luckyNumber  = document.querySelector("#lucky-number");
 const CheckButton = document.querySelector("#check-number");
+const displayMessage = document.querySelector("output-container");
+const calculatedSum =(date) => {
+    let sum=0;
+    date = date.replaceAll("-","");
+    for(let digit of date){
+        sum = sum + Number(digit);
+    }
+    return sum;
+};
+const checkIsNumberLucky = (sumOfDate ,numbertToCheck)=>{
+    console.log(sumOfDate,numbertToCheck);
+    if(sumOfDate % numbertToCheck==0){
+        return showMessage("ur birthday is lucky");
 
-function checkBirthDateIsLucky(){
-const date = dateofBirth.value;
-const sum = calculateSum(date);
+    }
+         showMessage("ur birthday is not lucky");
+    };
 
+
+const showMessage = (message)=>{
+displayMessage.innerText = message;
 }
-function calculateSum(){
-
-}
 
 
-
-
-
-CheckButton.addEventListener = ("clicked", checkBirthDateIsLucky)
-   
+CheckButton.addEventListener("click" , ()=>{
+    const date = dateofBirth.value;
+    const numbertToCheck = luckyNumber.value;
+    if(date&& numbertToCheck){
+        const sumOfDate = calculatedSum(date);
+checkIsNumberLucky(sumOfDate,numbertToCheck);
+    }
+    else{
+        showMessage("please enter both fields");
+    }
+});
