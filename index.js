998f3cc71aa7a51211cc76df9d1cd3cdd531d13e
muhdwaseem <waseemvann@gmail.com>
2021-08-28 17:30:17 +0530

    docs

---
+++ index.js
@@ -0,0 +1,19 @@
+const dateofBirth = document.querySelector("#date-of-birth");
+const luckyNumber  = document.querySelector("#lucky-number");
+const CheckButton = document.querySelector("#check-number");
+
+function checkBirthDateIsLucky(){
+const date = dateofBirth.value;
+const sum = calculateSum(date);
+
+}
+function calculateSum(){
+
+}
+
+
+
+
+
+CheckButton.addEventListener = ("clicked", checkBirthDateIsLucky)
+   
